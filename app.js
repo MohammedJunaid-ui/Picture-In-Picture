a3660f55a841ef4a5b33f6a91c7ca1f6b5134f7d
Mohammed Junaid Ap <junaidapmohammed@gmail.com>
2020-08-12 10:13:55 -0700

    files added

---
+++ app.js
@@ -0,0 +1,29 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Promp to select media stream, pass to video element, then play
+
+async function selectMediaStream () {
+    try{
+       const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+       videoElement.srcObject = mediaStream;
+       videoElement.onloadedmetadata = () => {
+           videoElement.play();
+       }
+    } catch (error) {
+        // Catch error here
+       console.log('whoops, error', error);
+    }
+}
+
+button.addEventListener('click', async() => {
+     //Disable Button
+     button.disabled = true;
+     // Start picture in picture
+     await videoElement.requestPictureInPicture(); 
+     //Reset Button
+     button.disabled = false; 
+});
+
+// On Load
+selectMediaStream();
